1c88bfa3b520e079eed81dfd2e8a0e669e4fb5a8
Molly Trepanier <mtrepanier123@gmail.com>
2020-12-17 19:49:56 +0000

    recommiting sice it didn't go before? again?

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
+// shows how f(x) works wherein you pass data/etc throuh the fucntion and can keep calling it
 const sayHello = function(name){
   console.log("Hello, " + name);
 }
@@ -5,3 +6,17 @@ const sayHello = function(name){
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+// shows diff. b/w consolelog and return-- console displays data, return returns data to be used
+const sayHelloToConsole = function(name) {
+  console.log("Hello, " + name);
+}
+
+sayHelloToConsole("John");
+
+const returnSayHello = function(name){
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
+
+
