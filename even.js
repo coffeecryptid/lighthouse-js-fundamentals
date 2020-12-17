0d3f56d30c5a68509293905ffc176a1c65c92dc7
Molly Trepanier <mtrepanier123@gmail.com>
2020-12-17 00:23:57 +0000

    more function exercies for lhl

---
+++ even.js
@@ -0,0 +1,18 @@
+// use of isEvent to show if somthing is even, outputs true/false
+/*
+const isEven = function(num) {
+  return num % 2 === 0;
+}
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+console.log(tenIsEven);
+console.log(elevenIsEven);
+*/
+// can further simplify by not putting result of f(x) to a variable, but just calling it directly
+
+const isEven = function(num) {
+  return num % 2===0
+}
+ console.log(isEven(10));
+ console.log(isEven(11));
+ 
